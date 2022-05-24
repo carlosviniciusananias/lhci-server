9920dcdb4b0ebca35d94d4de09491cff774a35d7
Carlos VInicius Ananias <carlos.oliveira69@fatec.sp.gov.br>
2022-05-24 19:45:37 -0300

    Added lhci local

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,10 +2,11 @@ module.exports = {
   ci: {
     collect: {
       url: ["https://loja.electrolux.com.br/"],
-      startServerCommand: "rails server -e production",
     },
     upload: {
-      target: "temporary-public-storage",
+      target: "lhci",
+      serverBaseUrl: "http://localhost:9001",
+      token: "777d08fe-6ef9-4cd4-980b-6cb690881297" // process.env.LHCI_TOKEN, // 7d378aad1b3844a7a8813ed1eddf3c3e3bf5f0c9f6d0056a97e50ce8e6e3c7ca
     },
   },
 };
