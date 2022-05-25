81519290faf5246e8e74bf5dfd5ef34857906306
Carlos VInicius Ananias <carlos.oliveira69@fatec.sp.gov.br>
2022-05-25 00:11:59 -0300

    Added new files

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,13 @@ module.exports = {
       url: ["https://loja.electrolux.com.br/"],
       startServerCommand: "rails server -e production",
     },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'uses-rel-preload': 'off',
+        'uses-rel-preconnect': 'off',
+      },
+    },
     upload: {
       target: "temporary-public-storage",
     },
