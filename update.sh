e1228379b39234792b63ed3529b51cd78b07bdae
Carlos VInicius Ananias <carlos.oliveira69@fatec.sp.gov.br>
2022-05-25 00:07:11 -0300

    Added new files

--- update.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-NEXT_VERSION= $( yarn info @lhci/server | grep ' last : ' -A 1 | tail -n 1 | grep -o " '.*' "  | sed s/ \' //g )
-node write-version.js " $NEXT_VERSION "
-
-git add -A
-git commit -m ' chore: atualize o servidor lhci '
-git push heroku heroku-deploy:main
